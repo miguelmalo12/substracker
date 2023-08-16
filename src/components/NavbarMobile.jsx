a2dba4408fdb7c5696fad94b5a9a3c32c0b04ea3
miguelmalo12 <miguelmalo12@gmail.com>
2023-08-15 19:46:49 -0700

    Subscriptions empty, mobile view finished, including menu

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -1,14 +1,12 @@
 import { ReactComponent as BurgerMenu } from "../assets/icons/burger_menu.svg";
 
-
-
-function Navbar({ content }) {
+function NavbarMobile({ content, toggleMenu }) {
   return (
     <header className="mb-4">
       {/* Mobile View Navbar */}
       <div className="md:hidden">
         <nav className="flex items-center justify-between w-full">
-          <BurgerMenu className="cursor-pointer"/>
+          <BurgerMenu className="cursor-pointer" onClick={toggleMenu} />
           <h1 className="content-center">{content}</h1>
           <div></div>
         </nav>
@@ -30,4 +28,4 @@ function Navbar({ content }) {
   );
 }
 
-export default Navbar;
+export default NavbarMobile;
--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -1,31 +1,63 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from "react";
 
-import NavbarApp from "../components/NavbarApp";
+import NavbarMobile from "../components/NavbarMobile";
+import MenuMobile from "../components/MenuMobile";
 import DropdownFilter from "../components/DropdownFilter";
 import ButtonSmall from "../components/ButtonSmall";
 import NoSubs from "../components/NoSubs";
 
 function Subscriptions() {
-    const [selectedInterval, setSelectedInterval] = useState('Monthly');
-    const [selectedMetric, setSelectedMetric] = useState('Average');
-  
-    return (
+  const [selectedInterval, setSelectedInterval] = useState("Monthly");
+  const [selectedMetric, setSelectedMetric] = useState("Average");
+
+  const menuRef = useRef(null);
+  const [isMenuVisible, setMenuVisible] = useState(false);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuVisible(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]);
+
+  return (
     <div className="responsive-padding">
-        <NavbarApp content={'Subscriptions'} />
+      <NavbarMobile
+        content={"Subscriptions"}
+        toggleMenu={() => setMenuVisible(!isMenuVisible)}
+      />
+      <div ref={menuRef}>
+        {isMenuVisible && <MenuMobile activePage="subscriptions" />}
+      </div>
+      <div className="flex gap-2.5 justify-between">
         <div className="flex gap-2.5">
-            <DropdownFilter options={['Monthly', 'Yearly', 'Weekly']} 
-            onChange={setSelectedInterval} />
-            <DropdownFilter options={['Average', 'Total']} onChange={setSelectedMetric}/>
-            <ButtonSmall content={"+ Add"} type={"primary"} />
+          <DropdownFilter
+            options={["Monthly", "Yearly", "Weekly"]}
+            onChange={setSelectedInterval}
+          />
+          <DropdownFilter
+            options={["Average", "Total"]}
+            onChange={setSelectedMetric}
+          />
         </div>
-        <div className="flex flex-col items-center">
-            <h1 className="py-2 text-4xl">0.00 $</h1>
-            <h4 className="text-medium-grey">{selectedInterval} {selectedMetric}</h4>
-        </div>
-        <div className="mt-6 mb-6 border"></div>
-        {/* <NoSubs /> */}
+        <ButtonSmall content={"+ Add"} type={"primary"} />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="py-2 text-4xl">0.00 $</h1>
+        <h4 className="text-medium-grey">
+          {selectedInterval} {selectedMetric}
+        </h4>
+      </div>
+      <div className="mt-6 mb-6 border"></div>
+      <NoSubs />
     </div>
-  )
+  );
 }
 
-export default Subscriptions
+export default Subscriptions;
