89765c813665f7befc18f85f289453ab472a7f37
miguelmalo12 <miguelmalo12@gmail.com>
2023-08-21 21:30:26 -0700

    Preferred currency coming from user ID get request

--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 import NavbarMobile from "../components/NavbarMobile";
 import MenuMobile from "../components/MenuMobile";
@@ -10,16 +12,63 @@ import Filters from "../components/Filters";
 import NoSubs from "../components/NoSubs";
 import Footer from "../components/Footer";
 import NavSubsDesktop from "../components/NavSubsDesktop";
+import Card from "../components/Card";
+
+const baseURL = process.env.REACT_APP_BASE_URL;
 
 function Subscriptions({ isMenuVisible, setMenuVisible, menuRef }) {
+  const navigate = useNavigate();
+
   const [selectedInterval, setSelectedInterval] = useState("Monthly");
   const [selectedMetric, setSelectedMetric] = useState("Average");
 
-  const navigate = useNavigate();
+  const [subscriptions, setSubscriptions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [preferredCurrency, setPreferredCurrency] = useState("CAD");
+  const [totalAmount, setTotalAmount] = useState(0);
 
   const handleAddClick = () => {
     navigate("/add-subscription");
   };
+
+  //GET Subscriptions
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/api/subscriptions/`)
+      .then((response) => {
+        if (response.data && Array.isArray(response.data.subscriptions)) {
+          setSubscriptions(response.data.subscriptions);
+          console.log(response.data.subscriptions);
+        } else {
+          console.error("Unexpected data format:", response.data);
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching subscriptions:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  //GET user's preferred currency
+  useEffect(() => {
+    // Using hardcoded user_id for now, needs to come from AUTH
+    const userId = 'test-uuid';
+
+    axios
+      .get(`${baseURL}/api/users/${userId}`)
+      .then((response) => {
+        if (response.data && response.data.user) {
+          setPreferredCurrency(response.data.user.preferred_currency);
+        } else {
+          console.error("Unexpected data format:", response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+      });
+  }, []);
 
   return (
     <main className="responsive-padding md:pl-28 md:min-h-screen md:flex md:flex-col">
@@ -68,7 +117,7 @@ function Subscriptions({ isMenuVisible, setMenuVisible, menuRef }) {
             />
           </div>
           <div className="flex flex-col items-center">
-            <h1 className="py-2 text-4xl">0.00 $</h1>
+            <h1 className="py-2 text-4xl">{totalAmount.toFixed(2)} {preferredCurrency}</h1>
             <h4 className="text-medium-grey">
               {selectedInterval} {selectedMetric}
             </h4>
@@ -77,7 +126,24 @@ function Subscriptions({ isMenuVisible, setMenuVisible, menuRef }) {
           <Filters />
         </section>
 
-        <NoSubs />
+        <div className="flex flex-wrap gap-3 md:gap-9">
+          {!loading && subscriptions.length === 0 ? (
+            <NoSubs />
+          ) : (
+            subscriptions.map((subscription) => (
+              <Card
+                key={subscription.subscription_id}
+                imageContent={subscription.logo}
+                name={subscription.name}
+                selectedCurrency={subscription.currency}
+                amount={subscription.amount}
+                recurrence={subscription.recurrence}
+                nextPaymentDate={subscription.payment_date}
+                color={subscription.color}
+              />
+            ))
+          )}
+        </div>
       </div>
       <Footer />
     </main>
